c5919e118da58cbb23907faf70f047e589c02815
Kabu <t.kaburagi@me.com>
2023-10-06 17:12:15 +0900

    Update demo-2.sh

--- demo-2.sh
+++ demo-2.sh
@@ -1,23 +1,27 @@
 #!/bin/sh
 
+ENDPOINT="http://localhost:8080/health"
+
 echo ${HELLO_KEY} > kv.txt
+
+java -jar myapp-0.0.1-SNAPSHOT.jar
 
 # 起動中のメッセージを表示
 echo "起動中です..."
 
-# アプリケーションが8080ポートで応答するまで待機
-while ! nc -z localhost $PORT; do
+# アプリケーションが起動するまで待機
+while true; do
+  # HTTPリクエストを送信してHTTPコードを取得
+  HTTP_CODE=$(curl -s -o /dev/null -w "%{http_code}" $ENDPOINT)
+
+  # HTTPコードが200の場合、起動完了とみなす
+  if [ $HTTP_CODE -eq 200 ]; then
+    echo "アプリケーションが起動しました。"
+    break
+  fi
+
+  # 1秒待機して再試行
   sleep 1
 done
 
 rm kv.txt
-
-# 削除が正常に行われたかを確認
-if [ $? -eq 0 ]; then
-  echo "kv.txtファイルが正常に削除されました。"
-else
-  echo "kv.txtファイルの削除に失敗しました。"
-fi
-
-# 削除後のディレクトリ内容を表示
-ls
